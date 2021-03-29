26e3f662ca0e363668c2f1beed0814157d5dd2d9
Erik Stambaugh <erik@erikstambaugh.com>
2021-03-29 08:13:54 -0700

    Allow startup.sh to accept arguments

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+CMD=$(1:-all}
+shift
+
 source /opt/mycroft/.venv/bin/activate
-/opt/mycroft/./start-mycroft.sh all
+/opt/mycroft/./start-mycroft.sh ${CMD} $@
 
 tail -f /var/log/mycroft/*.log
